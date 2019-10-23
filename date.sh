5ca027338d18a02d3941099fad465e523f38d954
ninuocheng <53173503+ninuocheng@users.noreply.github.com>
2019-10-23 09:27:10 -0500

    Create date.sh

---
+++ date.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# 打印各种时间格式  
+echo "显示星期简称(如:Sun)"
+date +%a
+echo "显示星期全称(如:Sunday)"
+date +%A
+echo "显示月份简称(如:Jan)"
+date +%b
+echo "显示月份全称(如:January)"
+date +%B
+echo "显示数字月份(如:12)"
+date +%m
+echo "显示数字日期(如:01 号)"
+date +%d
+echo "显示数字年(如:01 号)"
+date +%Y echo "显示年‐月‐日"
+date +%F
+echo "显示小时(24 小时制)"
+date +%H
+echo "显示分钟(00..59)"
+date +%M
+echo "显示秒"
+date +%S
+echo "显示纳秒"
+date +%N
+echo "组合显示"
+date +"%Y%m%d %H:%M:%S"
